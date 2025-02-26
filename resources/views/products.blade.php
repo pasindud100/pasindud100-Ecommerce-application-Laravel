049ca4590ac7e104e48b515e9c824dfd900f958d
Pasindu Dilshan <dilshanp5400@gmail.com>
2025-02-26 11:56:41 +0530

    Updated the product card UI.

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,12 +1,19 @@
 @extends('layouts.default')
 @section("title","Ecom - Home")
 @section("content")
-    <main class="container">
+    <main class="container" style="max-width:900px">
         <section>
-            @foreach($products as $product)
-                <p>{{$product->title}}</p>
-                
-            @endforeach
+        <div class="row">
+                @foreach($products as $product)
+                <div class="  col-12 col-md-6 col-lg-3">
+                    <div class="card p-2 shadow-sm">
+                        <img src="{{$product->image}}" width="100%" alt="">
+                        {{-- create title clickable --}}
+                        <div class=""><a href="">{{$product->title}}</a> | <span>LKR {{$product->price}}</span></div> 
+                    </div>
+                </div>
+                @endforeach            
+        </div>
         </section>
 
     </main>
