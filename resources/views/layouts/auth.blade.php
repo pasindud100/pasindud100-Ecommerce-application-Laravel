57aa7c28834b31c9008efdcebcd8d1055701c802
Pasindu Dilshan <dilshanp5400@gmail.com>
2025-02-26 06:25:37 +0530

    Integrated Bootstrap for styling.

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SVD e-commerce</title>
+    <link rel="stylesheet" href="{{assets("assets/css/bootstrap.min.css")}}">
+    @yield("style")
+</head>
+<body class="d-flex aliign-items-center py-4 bg-body-tertiary">
+    {{-- This for the login and registration --}}
+    @yield("content")
+    <script src="{{assets("assets/js/bootstrap.min.js")}}"></script>
+    @yield("script")
+    
+</body>
+</html>
